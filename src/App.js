9a09debb490e89722449cb01cffce5b58a39f588
Benjamin Wong <chiyeonwastaken@gmail.com>
2022-03-23 14:20:34 -0700

    now loads last searched weather location from local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const api = {
   key: process.env.REACT_APP_API_KEY,
@@ -8,18 +8,32 @@ const api = {
 function App() {
 
   const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+  /* retrieve previous location from local storage */
+  useEffect(() => {
+    var storedLocation = localStorage.getItem("prevLocation");
+    if(storedLocation && storedLocation !== "") {
+      updateWeather(storedLocation);
+    }
+  }, [])
+
+  const updateWeather = (q) => {
+    fetch(`${api.base}weather?q=${q}&units=imperial&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
+          localStorage.setItem("prevLocation", q);
           setWeather(result);
           setQuery('');
         });
+  }
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      updateWeather(query)
     }
   }
+  
 
   const GetDate = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
